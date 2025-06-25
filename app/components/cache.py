718ff186f42b41f0c93768c9a1e4966055e3c210
benjamin <benjaminhucko@gmail.com>
2025-06-25 21:41:48 +0200

    increased timeout to 8000h

--- app/components/cache.py
+++ app/components/cache.py
@@ -3,5 +3,5 @@ from flask_caching import Cache
 cache = Cache(config={
     'CACHE_TYPE': 'filesystem',
     "CACHE_DIR": "./tmp/flask_cache",
-    'CACHE_DEFAULT_TIMEOUT': 3e10,
+    'CACHE_DEFAULT_TIMEOUT': 3e7,
 })
